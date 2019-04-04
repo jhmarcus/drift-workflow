9c86071e6544279af9a93aec63a8d32439328d8c
Joseph Marcus <jhmarcus@uchicago.edu>
2019-04-04 11:36:29 -0500

    updated tree simulations

--- code/scrm2vcf.py
+++ code/scrm2vcf.py
@@ -9,11 +9,19 @@ for line in sys.stdin:
 
     # exctract pop info / sample sizes and write vcf header
     if i == 0:
+        # TODO: convert to regular expressions to 
+        # extract the correct params. Right now it 
+        # is hard-coded given the command line I use
+
         # number of haploid samples
         n_sam = int(line.split(" ")[1])
 
         # number of individuals
         n_ind = int(n_sam / 2)
+
+        # TODO: assuming hardcoded position of -r
+        region_size = int(line.split(" ")[7])
+        region_start = 0
 
         # write the vcf header
         sys.stdout.write("##fileformat=VCFv4.2\n")
@@ -26,18 +34,16 @@ for line in sys.stdin:
         header += "\n"
         sys.stdout.write(header)
 
-    # tally the chromosome
+    # new region so add the size of the next replicate
     if line.startswith("position"):
-        chrom += 1
-        #assert chrom <= 22, "Cant have more than 22 replicates"
+        region_start += region_size
 
-    # write a mutation
+    # write mutation
     if (len(lst) == n_sam + 2) and (not line.startswith("position")):
-        # modified from 
-        # https://github.com/popgenmethods/smcpp/blob/master/util/scrm2vcf.py
+        # modified from https://github.com/popgenmethods/smcpp/blob/master/util/scrm2vcf.py
         pos, time = lst[:2]
         gts = lst[2:]
-        cols = [str(chrom), str(int(float(pos))), ".", "A", "C", ".", "PASS", ".", "GT"]
+        cols = ["1", str(region_start + int(float(pos))), ".", "A", "C", ".", "PASS", ".", "GT"]
         cols += ["|".join(gt) for gt in zip(gts[::2], gts[1::2])]
         sys.stdout.write("\t".join(cols) + "\n")
     
