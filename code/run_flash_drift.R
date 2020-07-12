5e8b23a569e8ddacdf5568b027d0f4be9b093fc6
Joseph Marcus <jhmarcus@uchicago.edu>
2020-07-12 11:52:41 -0500

    started 1kg rmd

--- code/run_flash_drift.R
+++ code/run_flash_drift.R
@@ -2,16 +2,16 @@ library(tidyverse)
 library(ebnm)
 library(flashier)
 library(drift.alpha)
+options(extrapolate.control = list(beta.max=1.0))
 
 args = commandArgs(trailingOnly=TRUE)
 greedy_rds_path= args[1]
 drift_rds_path = args[2]
 
 greedy_flash_fit = readRDS(greedy_rds_path)
-# run drift
-fl <- drift(init_from_flash(greedy_flash_fit), 
-            miniter=500, 
-            maxiter=2500, tol=0.001, verbose=TRUE)
+init <- init_from_flash(greedy_flash_fit)
+dr <- drift(init, verbose=TRUE, extrapolate=FALSE, maxiter=20, tol=1e-5)
+dr2 <- drift(dr, verbose=TRUE, extrapolate=TRUE, maxiter=2500, tol=1e-5)
 
 # save the rds
-saveRDS(fl, drift_rds_path)
+saveRDS(dr2, drift_rds_path)
