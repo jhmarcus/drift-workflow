1fed6f7a5d999b3e91dd7596698def9ef5a249ad
Joseph Marcus <jhmarcus@uchicago.edu>
2020-07-15 13:40:06 -0500

    1kg analysis updates

--- code/structure_plot.R
+++ code/structure_plot.R
@@ -60,7 +60,7 @@ create_structure_plot <- function(L, labels, colors,
                  axis.text.x=element_text(size = label_font_size, colour = "black", angle = 45, hjust = 1), 
                  axis.ticks.x=element_blank(),
                  axis.line.x = element_blank(),
-                 plot.margin = unit(c(0, 0, 0, 0), "cm"),
+                 plot.margin = unit(c(0.0, 0, 0.0, 0), "cm"),
                  axis.title.y=element_text(size=yaxis_title_font_size)) + 
            ylab("Loading") + 
            guides(fill=FALSE, color=FALSE) +
